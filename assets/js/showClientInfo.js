31e89720eb9e0f3c0669633287c271f9025f9e48
NeProgramist <zasko180@gmail.com>
2020-05-05 18:15:32 +0300

    add client info

---
+++ assets/js/showClientInfo.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const showClientInfo = () => document.getElementsByClassName("client")[0].style.display = "flex";
+const hideClientInfo = () => document.getElementsByClassName("client client_size-max")[0].style.display = "none";
