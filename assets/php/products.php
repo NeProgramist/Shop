9dfa4198fc8d7087d169ed157df968fa13e26e91
NeProgramist <zasko180@gmail.com>
2020-05-29 00:45:34 +0300

    add opportunity to find diff types of products in sql

--- assets/php/products.php
+++ assets/php/products.php
@@ -1,6 +1,23 @@
 <?php
+error_reporting(0);
 require_once 'database.php';
-$query ="SELECT * FROM shop.products";
+
+$type = $_GET["type"];
+$search = $_GET["search"];
+$ids = $_GET["ids"];
+$query = "SELECT * FROM shop.products";
+
+if ($ids != "") {
+    $query .= " WHERE product_id in ($ids)";
+} else if($search != "") {
+    $query .= " WHERE name = '$search'";
+} else if($type == 0) {
+    $query .= "";
+} else {
+    $query .= " WHERE type = '$type'";
+}
+
+
 
 $result = mysqli_query($link, $query) or die("Cannot get data: " . mysqli_error($link));
 
